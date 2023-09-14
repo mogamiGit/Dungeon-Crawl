4086f29d71d5577bea73b82275baa20c5b6ad2e5
Monica Galan <mogami.creative@gmail.com>
2023-09-14 03:55:30 +0200

    Initial Commit

---
+++ Dungeon Crawl/Dungeon_CrawlApp.swift
@@ -0,0 +1,32 @@
+//
+//  Dungeon_CrawlApp.swift
+//  Dungeon Crawl
+//
+//  Created by Monica Galan de la Llana on 14/9/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Dungeon_CrawlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
